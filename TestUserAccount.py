6b59e54b531483d2159fe0c85713806135a1fcf4
mahmood18-meet <mahmood.z143@gmail.com>
2016-12-14 19:56:54 +0200

    working on examples for the class

--- TestUserAccount.py
+++ TestUserAccount.py
@@ -2,12 +2,12 @@
 from UserAccount import UserAccount
 
 #Two things are missing from the line below - fill them in
-my_user=UserAccount(  , ,  )
+my_user=UserAccount("mahmood","meetyear18","i am 15 years old")
 
 #Call the print_secret method (function) - it takes one input - a guess for the password.
-
 #Use the wrong password as input here
-my_user.
+my_user.print_secret("meet")
 
 #Use the right password here
-my_user.
+my_user.print_secret("meetyear18")
+
